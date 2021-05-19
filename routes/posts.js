abaeeb49eb8e064aee200b65566394d648db95f6
Dominique Norris <github.com/domino31620/domino31620>
2021-05-19 16:16:20 -0700

    initialize posts

---
+++ routes/posts.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+
+
+router.post('/',function(req,res,next){
+
+})
+router.get('/', function(req,res,next){
+
+})
+router.get('/:id', function(req,res,next){
+  
+})
+router.put('/:id', function(req,res,next){
+  
+})
+module.exports = router;
